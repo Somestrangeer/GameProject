164b088846e4f5ff65232d34d960e642b1e958ae
Alexander <95109481+Somestrangeer@users.noreply.github.com>
2024-02-24 23:39:48 +0300

    Переделана локация, добавлены анимации героя и камера

--- Game2D/Assets/hero.cs
+++ Game2D/Assets/hero.cs
@@ -1,14 +1,22 @@
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class hero : MonoBehaviour
 {
     // Start is called before the first frame update
     private GameObject herpObj;
+    private Animator heroAnima;
+    private bool isMovingLeft;
+    private bool isMovingRight;
+    private bool isMovingUp;
+    private bool isMovingDown;
     void Start()
     {
         herpObj = gameObject;
+        heroAnima = herpObj.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -16,10 +24,49 @@ public class hero : MonoBehaviour
     {
         Vector3 movement = Vector3.zero;
 
-        if (Input.GetKey(KeyCode.D)) { movement += new Vector3(3, 0, 0); }
-        if (Input.GetKey(KeyCode.A)) { movement -= new Vector3(3, 0, 0); }
-        if (Input.GetKey(KeyCode.W)) { movement += new Vector3(0, 3, 0); }
-        if (Input.GetKey(KeyCode.S)) { movement -= new Vector3(0, 3, 0); }
+        if (Input.GetKey(KeyCode.D) && !isMovingLeft && !isMovingUp && !isMovingDown)
+        {
+            heroAnima.Play("Right Animation");
+            movement += new Vector3(3, 0, 0);
+            isMovingRight = true;
+        }
+        if (Input.GetKey(KeyCode.A) && !isMovingRight && !isMovingUp && !isMovingDown)
+        {
+            heroAnima.Play("Left Animation");
+            movement -= new Vector3(3, 0, 0);
+            isMovingLeft = true;
+        }
+        if (Input.GetKey(KeyCode.W) && !isMovingDown && !isMovingLeft && !isMovingRight)
+        {
+            heroAnima.Play("Up Animation");
+            movement += new Vector3(0, 3, 0);
+            isMovingUp = true;
+        }
+        if (Input.GetKey(KeyCode.S) && !isMovingUp && !isMovingLeft && !isMovingRight)
+        {
+            heroAnima.Play("Down Animation");
+            movement -= new Vector3(0, 3, 0);
+            isMovingDown = true;
+        }
+
+        // Для остановки анимации при отпускании клавиши
+        if (!Input.GetKey(KeyCode.D))
+        {
+            isMovingRight = false;
+        }
+        if (!Input.GetKey(KeyCode.A))
+        {
+            isMovingLeft = false;
+        }
+        if (!Input.GetKey(KeyCode.W))
+        {
+            isMovingUp = false;
+        }
+        if (!Input.GetKey(KeyCode.S))
+        {
+            isMovingDown = false;
+        }
+
 
         // To make an animation we have to multiply it by time
         herpObj.transform.position += movement * Time.deltaTime;
