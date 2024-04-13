3dad2557296de96034ddefa446ce03d531d07529
Alexander <95109481+Somestrangeer@users.noreply.github.com>
2024-04-13 20:34:14 +0300

    Доделана локация Храма

---
+++ Game2D/Assets/Scripts/OpenDoorTemple.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoorTemple : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    GameObject hero;
+    GameObject door;
+    Animator doorAnima;
+
+    void Start()
+    {
+        hero = GameObject.FindGameObjectWithTag("Hero");
+        door = GameObject.FindGameObjectWithTag("Door");
+
+        doorAnima = door.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.E)) 
+        {
+            Debug.Log("Here1");
+            if (Vector3.Distance(hero.transform.position, door.transform.position) < 6) 
+            {
+                Debug.Log("Here2");
+                doorAnima.SetBool("openDoor", true);
+                BoxCollider2D boxCollider = door.GetComponentInChildren<BoxCollider2D>();
+                boxCollider.enabled = false;
+            }
+        }
+        
+    }
+}
